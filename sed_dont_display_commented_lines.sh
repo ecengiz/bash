e1a65604c97f6e237fe7faf07c9ca029f0d6a091
ecengiz <me@emrecengiz.com>
2023-04-14 16:10:57 -0500

    Update sed_dont_display_commented_lines.sh

--- sed_dont_display_commented_lines.sh
+++ sed_dont_display_commented_lines.sh
@@ -1,1 +1,4 @@
 sed '/^\s*#/d' x.txt
+
+# /pattern/d - deletes any line that matches the given pattern.
+# ^\s*# - matches any line that starts with zero or more whitespace characters followed by a #.
